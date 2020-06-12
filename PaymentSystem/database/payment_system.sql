141500541aa81bd5b844c860bd955e3b11c0389e
Vladimir Smolyaninov <vlad.smolyaninov_va@list.ru>
2020-06-12 22:19:47 +1000

    описание проекта

--- PaymentSystem/database/payment_system.sql
+++ PaymentSystem/database/payment_system.sql
@@ -1,12 +1,34 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июн 12 2020 г., 15:11
+-- Версия сервера: 5.7.16
+-- Версия PHP: 7.0.14
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- База данных: `payment_system`
+--
+CREATE DATABASE IF NOT EXISTS `payment_system` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `payment_system`;
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `payments`
+--
+
+DROP TABLE IF EXISTS `payments`;
 CREATE TABLE `payments` (
   `id` int(11) NOT NULL,
   `appointment` varchar(100) NOT NULL,
@@ -15,6 +37,10 @@ CREATE TABLE `payments` (
   `date` datetime NOT NULL,
   `sessionId` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `payments`
+--
 
 INSERT INTO `payments` (`id`, `appointment`, `card_Number`, `total`, `date`, `sessionId`) VALUES
 (63, 'asdasdasd', '000000000000000000', '11.00', '2020-06-12 12:14:08', NULL),
@@ -32,11 +58,23 @@ INSERT INTO `payments` (`id`, `appointment`, `card_Number`, `total`, `date`, `se
 (75, '123', '000000000000000000000', '-213123.00', '2020-06-12 13:38:12', 'pay-d09bf41544a3365a46c9077ebb5e35c3'),
 (76, '123', '000000000000000000000', '213123.00', '2020-06-12 13:39:33', 'pay-fbd7939d674997cdb4692d34de8633c4');
 
+--
+-- Индексы сохранённых таблиц
+--
 
+--
+-- Индексы таблицы `payments`
+--
 ALTER TABLE `payments`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
 
+--
+-- AUTO_INCREMENT для таблицы `payments`
+--
 ALTER TABLE `payments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
